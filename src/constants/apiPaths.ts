fdc99c69b3c6b10c78c731ef90960e043ab30311
Nikita Staroseltsev <nikitozz_13@mail.ru>
2023-03-12 23:57:13 +0300

    TASK-5 Add imports file api endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://flw23890r2.execute-api.eu-west-2.amazonaws.com/dev",
   order: "https://flw23890r2.execute-api.eu-west-2.amazonaws.com/dev",
-  import: "https://flw23890r2.execute-api.eu-west-2.amazonaws.com/dev",
+  import: "https://2tubbygjn2.execute-api.eu-west-2.amazonaws.com/dev",
   bff: "https://flw23890r2.execute-api.eu-west-2.amazonaws.com/dev",
   cart: "https://flw23890r2.execute-api.eu-west-2.amazonaws.com/dev",
 };
